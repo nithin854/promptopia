55777050d8e6594867f6042480362c3638ee3498
nithin854 <41363398+nithin854@users.noreply.github.com>
2024-03-19 17:20:54 +0530

    Add files via upload

---
+++ models/prompt.js
@@ -0,0 +1,21 @@
+import {model,Schema,models} from "mongoose";
+
+const PromptSchema = ({
+    creator : {
+        type : Schema.Types.ObjectId,
+        ref : 'User'
+    },
+    prompt : {
+        type : String,
+        required : [true,"Prompt is required"]
+    },
+    tag : {
+        type : String,
+        required : [true,"Tag is required"],
+    }
+
+})
+
+const Prompt = models.Prompt || model("Prompt", PromptSchema);
+
+export default Prompt;
